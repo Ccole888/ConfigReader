2c42d90cbe239020b6b122eec252c01ae4f72e15
Ccole888 <colincole888@gmail.com>
2023-09-03 12:28:34 -0400

    Update readconfig.java

---
+++ readconfig.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class readconfig{
+    public static void main(String[] args){
+
+    }
+
+    public HashMap readconfigFile(ArrayList<String> configParams){
+        HashMap<String, String> returnvalues = new HashMap<String, String>();
+        
+        return returnvalues;
+    }
+}
